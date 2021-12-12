775db9d7e633f7b86d32be9f9ee36a59f045d729
mrn720 <matt.ryan.norris@gmail.com>
2021-12-11 18:47:06 -0600

    MVP

--- api/server.js
+++ api/server.js
@@ -1,13 +1,20 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const helmet = require('helmet');
+const { logger } = require('./middleware/middleware.js');
 const server = express();
+const usersRouter = require('./users/users-router.js');
 
 // remember express by default cannot parse JSON in request bodies
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
+server.use(logger)
+server.use('/api/users', usersRouter)
 
 // global middlewares and the user's router need to be connected here
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
-
 module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it
+const express = require('express')
+const server = require('./api/server')
+
+server.listen(9000, () => {
+    console.log("Server listening on port 9000")
+})
